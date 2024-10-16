30491c7611d0ba58fb17b1a6bda13fd0e0ef74ae
Colton Hurst <colton@coltonhurst.com>
2024-10-16 14:40:25 -0400

    Add initial index.ts code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-import * as fs from 'fs';
+import { BitwardenClient, ClientSettings, DeviceType, LogLevel } from "@bitwarden/sdk-napi";
 
-fs;
+const organizationId = "organization-id-here";
+const accessToken = "access-token-here";
+const stateFile = "state-file-path-here";
+
+const settings: ClientSettings = {
+  apiUrl: "http://localhost:4000",
+  identityUrl: "http://localhost:33656",
+  userAgent: "Bitwarden SDK",
+  deviceType: DeviceType.SDK,
+};
+
+// Build the Client
+const client = new BitwardenClient(settings, LogLevel.Info);
+
+// Authenticate using a machine account access token
+await client.auth().loginAccessToken(accessToken, stateFile);
+
+// List secrets
+const secrets = await client.secrets().list(organizationId);
+console.log(secrets);
+
+// Get a specific secret
+const secret = await client.secrets().get("secret-id-here");
+console.log(secret);
